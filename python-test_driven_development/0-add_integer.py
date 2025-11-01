c346c5c3833b0529eb23e03993d499bc0bc00412
MahdiTaghizade <tagizademehti@gmail.com>
2025-11-01 20:08:08 +0400

    a

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,43 +1,17 @@
 #!/usr/bin/python3
-"""This module defines a function that adds two integers.
-The function ensures the arguments are integers or floats,
-casts floats to integers before addition, and raises
-appropriate exceptions when needed.
-"""
+"""Defines an integer addition function."""
 
 
 def add_integer(a, b=98):
-    """Add two integers or floats and return the result as an integer.
+    """Return the integer addition of a and b.
 
-    Args:
-        a (int or float): first number
-        b (int or float, optional): second number (default = 98)
-
-    Returns:
-        int: the sum of a and b after casting to integers.
+    Float arguments are typecasted to ints before addition is performed.
 
     Raises:
-        TypeError: if a or b are not integers or floats
-        OverflowError: if a or b are NaN or infinity
+        TypeError: If either of a or b is a non-integer and non-float.
     """
-    if type(a) not in (int, float):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if type(b) not in (int, float):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-
-    a = convert_to_int(a)
-    b = convert_to_int(b)
-    return a + b
-
-
-def convert_to_int(num):
-    """Convert a float to an int, checking for NaN or infinity."""
-    if type(num) is float:
-        # check NaN (NaN != NaN)
-        if num != num:
-            raise OverflowError("cannot convert float NaN to integer")
-        # check infinity (+inf or -inf)
-        if num == float("inf") or num == float("-inf"):
-            raise OverflowError("cannot convert float infinity to integer")
-        num = int(num)
-    return num
+    return (int(a) + int(b))
