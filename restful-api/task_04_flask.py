3d6c8e1df69bf40ff06c9c16eb27e3416b24249d
MahdiTaghizade <tagizademehti@gmail.com>
2025-11-15 21:20:28 +0400

    vahg

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -19,15 +19,22 @@ def data():
 @app.route('/add_user', methods=['POST'])
 def add_user():
     data = request.json
-    if data is None or data.get('username') is None:
+    if data is None:
+        return jsonify({'error': 'Invalid JSON'}), 400
+    if data.get('username') is None:
         return jsonify({'error': 'Username is required'}), 400
+
+    username = data.get('username')
+    if username in users:
+        return jsonify({'error': 'Username already exists'}), 409
+
     user = {
-        'username': data.get('username'),
+        'username': username,
         'name': data.get('name'),
         'age': data.get('age'),
         'city': data.get('city')
     }
-    users[user.get('username')] = user
+    users[username] = user
     return jsonify({'message': 'User added', 'user': user}), 201
 
 
