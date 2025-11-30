a72b18c2bee3a3601f078775fee1303417cde92c
MahdiTaghizade <tagizademehti@gmail.com>
2025-11-30 10:47:57 +0400

    fsd

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,2 +1,4 @@
 #!/usr/bin/node
-console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
